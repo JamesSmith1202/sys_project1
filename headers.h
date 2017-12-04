86a0ed7ff1e2c6f4bc1cd4b3a53572686dcde2f8
JamesSmith1202 <jsmith@stuy.edu>
2017-12-04 00:39:04 -0500

    added info to headers.h

--- headers.h
+++ headers.h
@@ -1,10 +1,10 @@
-void print_arr(char **);
-int count_delims(char* , char*);
-char ** parse_args(char * , char *, int );
-void fix_newline(char *);
-char * strip_spaces(char * );
-void redirect_in(char **, int);
-void redirect_out(char ** , int );
-void execute_child(char ** , int);
-void execute(int , char ** );
-void get_input(char ** );
+void print_arr(char **);//prints each element of the array
+int count_delims(char* , char*);//goes through the array and counts the number of delimeters in the string. Must be single char delimeters 
+char ** parse_args(char * , char *, int );//splits a string on the delimeter and returns the char of splits. int is the number of delims for memory allocation. use count_delims()
+void fix_newline(char *);//removes the newline from the end of the line on user input
+char * strip_spaces(char * );//trims the spaces on the left and right sides of a string
+void redirect_in(char **, int);//handles all redirections for standard in given the array of split commands and the length of the array.
+void redirect_out(char ** , int );//handles all redirections to standard out given the array of split commands and the length of the array
+void execute_child(char ** , int);//function that handles all command executions done by the child
+void execute(int , char ** );//General execution function that handles the parent executions(cd and exit) or forks a child and sends it to execute_child
+void get_input(char ** );//gets input from the user
